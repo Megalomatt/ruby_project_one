c718682f592f246208f3174caaf4daae904f6a01
Matt <colbournematt@gmail.com>
2018-07-31 17:48:58 +0000

    Added edit/update/show/index listing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get 'about', to: 'welcome#about'
+# get 'new_article', to: 'articles#new'
+# get 'edit_article', to: 'articles#edit'
+# get 'articles', to: 'articles'
   
   resources :articles
 end
