313faa88533fe65a26446df992a38d77e4ace1fd
Matt <colbournematt@gmail.com>
2018-08-04 04:21:24 +0000

    Added link restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#home'
-  get 'about', to: 'welcome#about'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 # get 'new_article', to: 'articles#new'
 # get 'edit_article', to: 'articles#edit'
 # get 'articles', to: 'articles'
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
